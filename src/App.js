0f1a3cd0d9cdf98680e8cadaadd33c9a4b57a51e
Parth-Modi <79822631+SherlockHolmes-dev@users.noreply.github.com>
2024-01-17 16:28:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import '@master/css';
+import {
+  PrimaryButton,
+  SecondaryButton,
+  RedButton,
+  YellowButton,
+  PurpleButton,
+  OrangeButton,
+  TealButton,
+} from './Button'; 
 
 const App = () => {
-  const handleButtonClick = (buttonText) => {
-    console.log(`${buttonText} clicked!`);
+  const handleButtonClick = () => {
+    console.log('Button clicked!');
   };
 
   return (
     <div>
-      <h1>React Button Example</h1>
-      <button className="bg:red color:white" onClick={() => handleButtonClick('Button 1')}>Button 1</button>
-      <button className="bg:blue color:white" onClick={() => handleButtonClick('Button 2')}>Button 2</button>
-      <button className="bg:black color:white" onClick={() => handleButtonClick('Button 3')}>Button 3</button>
+      <h1>React Reusable Button Example</h1>
+      <PrimaryButton onClick={handleButtonClick} text="Primary Button" />
+      <SecondaryButton onClick={handleButtonClick} text="Secondary Button" />
+      <RedButton onClick={handleButtonClick} text="Red Button" />
+      <YellowButton onClick={handleButtonClick} text="Yellow Button" />
+      <PurpleButton onClick={handleButtonClick} text="Purple Button" />
+      <OrangeButton onClick={handleButtonClick} text="Orange Button" />
+      <TealButton onClick={handleButtonClick} text="Teal Button" />
     </div>
   );
 };
